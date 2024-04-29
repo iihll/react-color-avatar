4707dd91b39259b5a7229849745b475fd9ca0565
xingduanqing <xingduanqing@58.com>
2024-04-29 11:10:21 +0800

    fix: footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -13,31 +13,31 @@ export function Footer() {
     <footer className="footer">
       <div
       data-message="If you are deploying to your own public website, please do not modify it unless you have permission from the original author."
-    >
-      <a
-        className="link"
-        href="https://blog.iihll.com"
-        target="_blank"
-        rel="nofollow noopener noreferrer"
       >
-      Made by iihll
-      </a>
-    </div>
+        <a
+          className="link"
+          href="https://blog.iihll.com"
+          target="_blank"
+          rel="nofollow noopener noreferrer"
+        >
+        Made by iihll
+        </a>
+      </div>
 
-    <div className="divider">|</div>
+      <div className="divider">|</div>
 
-    <div className="locale" onClick={switchLocale}>
-      {i18n.language === Locale.EN ? '简体中文' : 'English'}
-    </div>
+      <div className="locale" onClick={switchLocale}>
+        {i18n.language === Locale.EN ? '简体中文' : 'English'}
+      </div>
 
-    <div className="divider">|</div>
+      {/* <div className="divider">|</div> */}
 
-    <a
-      href="https://zeabur.com?referralCode=Codennnn&utm_source=Codennnn"
-      target="_blank"
-    >
-      <img src="https://zeabur.com/deployed-on-zeabur-dark.svg" />
-    </a>
+      {/* <a
+        href="https://zeabur.com?referralCode=Codennnn&utm_source=Codennnn"
+        target="_blank"
+      >
+        <img src="https://zeabur.com/deployed-on-zeabur-dark.svg" />
+      </a> */}
     </footer>
   )
 }
